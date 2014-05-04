9d8d60480824bfb5071491d46569c487feef20f5
Michael Klein <klein@monbro.de>
2014-05-04 14:09:03 +0100

    add basic requirements

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
 javascript-sdk-boilerplate
 ===========================
 
+Work in progress. This repository is not ready to use yet.
+
 A boilerplate code template for creating a generic javascript sdk.
+
+# Getting started
+
+Content missing.
+
+# Setup
+
+* run ```npm install```
+* run ```gulp``` to see the list of available tasks
+
+# Tutorials
+
+* http://msdn.microsoft.com/en-us/library/jj820239.aspx
+
+# How others do it:
+
+* https://github.com/apigee/apigee-javascript-sdk/blob/master/apigee.js
+* https://github.com/stackmob/stackmob-js-sdk/blob/master/stackmob.js
+* https://github.com/gilt/gilt-js-sdk/blob/master/gilt-js-api.js
+* https://github.com/BuddyPlatform/Buddy-JS-SDK/blob/master/buddy.js
+* https://github.com/EdmundsAPI/sdk-javascript/blob/master/edmunds.api.sdk.js
+* https://github.com/justintv/twitch-js-sdk/blob/master/twitch.js
+* https://github.com/aws/aws-sdk-js/blob/master/dist/aws-sdk.js
+* https://github.com/Instagram/instagram-javascript-sdk/blob/master/ig.js
+* https://github.com/splunk/splunk-sdk-javascript/blob/master/client/splunk.js
+
+# Final usage examples
+
+* https://keen.io/docs/clients/javascript/usage-guide/ with https://dc8na2hxrj29i.cloudfront.net/code/keen-2.1.0-min.js
+* https://developers.facebook.com/docs/javascript/quickstart/v2.0 with https://connect.facebook.net/en_US/sdk.js
+* https://parse.com/docs/js_guide with www.parsecdn.com/js/parse-1.2.18.min.js
---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+/**
+ * this is the main task runner for managing the SampleSDK
+ */
+
+/*
+ * define variables
+ * ***********************************************************************************************
+ */
+
+var gulp = require('gulp');
+var args   = require('yargs').argv;
+var gutil = require('gulp-util');
+var colours = require('colors');
+var fs = require('fs');
+var exec = require('child_process').exec;
+var sys = require('sys');
+
+var PROJECT_BASE_PATH = __dirname + '';
+
+/*
+ * main tasks
+ * ***********************************************************************************************
+ */
+
+// default task, run with 'gulp'
+gulp.task('default', function(){
+    console.log();
+    console.log('No default task defined.');
+    console.log('The following tasks are available:');
+
+    taskList = Object.keys(gulp.tasks);
+    taskList.splice( taskList.indexOf( "default" ), 1 ); // remove not needed item
+    for (var i = taskList.length - 1; i >= 0; i--) {
+        console.log('- '+('gulp '+taskList[i]).green);
+    }
+    console.log();
+});
+
+
+gulp.task('testrun', function(cb){
+    console.log();
+    console.log('Run all the tests now');
+    var karmaConfigFile = PROJECT_BASE_PATH+'/test/karma.conf.js';
+    var commandLine = 'karma start '+karmaConfigFile;
+    executeCommand(commandLine, cb);
+    console.log();
+});
+
+/*
+ * helper scripts
+ * ***********************************************************************************************
+ */
+
+// execute the command line command in the shell
+function executeCommand(commandLine, cb) {
+    exec(commandLine, function(error, stdout, stderr) {
+        puts(error, stdout, stderr);
+        cb(null); // will allow gulp to exit the task successfully
+    });
+}
+
+// well display console expressions
+function puts(error, stdout, stderr) {
+    sys.puts(stdout);
+}
